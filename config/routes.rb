08fe8209146cbcbb3fd2b3abb62354324bfef773
michael-duke <michael.21duke@gmail.com>
2023-01-04 20:29:58 +0300

    Generate devise sessions and registrations controllers for users - Configure routes under api/v1/ namespace - Add associations and validations to user model - Add application controller for api/v1/ namespace - Correct rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope :api, defaults: { format: :json } do
+    scope :v1 do 
+      devise_for :users, # => this is the devise_for :users that is used for the api
+      controllers: { 
+        registrations: 'api/v1/users/registrations', 
+        sessions: 'api/v1/users/sessions' 
+      },
+      path: '', 
+      path_names: { 
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'register'
+      }
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # API routes
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users, only: [:index, :show] do 
+  #       resources :bookings, only: [:index, :show]
+  #     end
+  #     resources :cars, only: [:index, :show]
+  #   end
+  # end
+  
+  # Defines the root path route ("/") to the cars#index action
+  #root to: 'api/v1/cars#index'
 end
