c50121659902708e3fbcc5f0d00b6b273a1d145a
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-04 22:03:40 +0300

    Update date columns for bookings schema ✨

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_181758) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_171944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "pick_up_date"
+    t.datetime "pickup_date"
     t.datetime "return_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
