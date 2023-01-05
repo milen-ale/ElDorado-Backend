baee461e4f5b5781c667b3c342e0186b01aea416
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-05 15:14:31 +0300

    Update bookings migration with user and car references 🔧

--- db/migrate/20230104171944_create_bookings.rb
+++ db/migrate/20230104171944_create_bookings.rb
@@ -3,6 +3,8 @@ class CreateBookings < ActiveRecord::Migration[7.0]
     create_table :bookings do |t|
       t.datetime :pickup_date
       t.datetime :return_date
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
+      t.references :car, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
