b2f88f07d1fd6dc0ec52a971fb05c6fcc7ff2d22
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-05 15:09:19 +0300

    Add validations & belongs to relation 🔗

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :car
+
+  validates :return_date, comparison: { greater_than: :pickup_date }
+  validates :pickup_date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :return_date, presence: true, comparison: { greater_than: Date.today }
 end
