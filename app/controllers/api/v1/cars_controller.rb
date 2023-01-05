7795e6c78f066f88223bf3b7b915f7d5ae509d92
milen-ale <milenalebel2@gmail.com>
2023-01-05 18:34:40 +0300

    fix linter errors

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::CarsController < ApplicationController
   before_action :set_car, only: %i[show update destroy]
- 
+
   def index
-    @cars = Car.all 
+    @cars = Car.all
     render json: @cars, status: :ok
   end
 
   def show
     render json: @car, status: :ok
-   end
+  end
 
   private
 
