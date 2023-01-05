11958ec5624a3243301bd5b2a930d800eea864bd
michael-duke <michael.21duke@gmail.com>
2023-01-05 11:47:37 +0300

    Adjust JWT dispatch and revocation requests urls

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,10 +312,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_SECRET_KEY']
     jwt.dispatch_requests = [
-      ["POST", %r{^/users/sign_in$}],
+      ["POST", %r{^/api/v1/login$}],
     ]
     jwt.revocation_requests = [
-      ["DELETE", %r{^/users/sign_out}],
+      ["DELETE", %r{^/api/v1/logout}],
     ]
     jwt.expiration_time = 120.minutes.to_i
   end
