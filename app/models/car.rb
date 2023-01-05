d2503019f452d6bf6c7c8d639b4acad6c02c1f1e
milen-ale <milenalebel2@gmail.com>
2023-01-05 17:25:36 +0300

    add spec for car model and follow TDD

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,9 +2,9 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :user, through: :bookings
 
-  validates :name, presence: true, length: { in: 1..250 }
-  validates :model, presence: true, length: { in: 1..250 }
-  validates :image, presence: true, length: { in: 1..250 }
+  validates :name, presence: true, length: { in: 4..250 }
+  validates :model, presence: true, length: { in: 4..250 }
+  validates :image, presence: true, length: { in: 4..250 }
   validates :daily_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :description, presence: true, length: { in: 1..500 }
+  validates :description, presence: true, length: { in: 5..500 }
 end
