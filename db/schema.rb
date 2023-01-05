f40e4eb2e9d7f2646ca008a994702c93554e27b6
michael-duke <michael.21duke@gmail.com>
2023-01-05 12:57:22 +0300

    Run new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_01_04_193644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "pickup_date"
+    t.datetime "return_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,13 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_193644) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "bookings", force: :cascade do |t|
-    t.datetime "pickup_date"
-    t.datetime "return_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
