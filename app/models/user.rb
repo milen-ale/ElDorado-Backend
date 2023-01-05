e371709436b2583f66943f6ab04459ac64bbc044
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-05 09:43:04 +0500

    Remove unwanted validation in the user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { in: 1..50 }, uniqueness: true
-  validates :password, presence: true, length: { in: 6..20 }
 
   # has_many :cars, dependent: :destroy
   # has_many :bookings, dependent: :destroy
