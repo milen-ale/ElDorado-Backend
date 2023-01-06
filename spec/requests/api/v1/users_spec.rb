59fb8d40cad5b117b81ff11b84c4699115b769ad
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-06 14:45:20 +0500

    Add integration specs for users

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,32 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/users', type: :request do
+
+ path '/api/v1/users/' do
+    get 'Show Current User' do
+      tags 'Current User'
+      produces 'application/json'
+      
+      response '200', 'Current User found' do
+        before do
+         @user = User.create(name: 'Michael Mesfin', email: 'michael@jedi.com', password: 'password', password_confirmation: 'password')
+         sign_in @user
+        end
+
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            name: { type: :string },
+            email: { type: :string }
+          },
+          required: [ 'id', 'name', 'email' ]
+        run_test!
+      end
+
+      response '401', 'Current user not found' do
+        let(:user) { 'invalid' }
+        run_test!
+      end
+    end
+  end
+end
