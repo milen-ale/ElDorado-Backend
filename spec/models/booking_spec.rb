fde4c4b2662657d812a8fab870586b64aee17c90
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-06 12:28:50 +0300

    Fix linter errors (line too long) 💊

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Booking, type: :model do
                   image: 'https://www.bmw.com/content/dam/bmw/common/all-models/3-series/sedan/2021/navigation/BMW-3-Series-Sedan-2021-Exterior-01.jpg/_jcr_content/renditions/cq5dam.resized.img.585.low.time1594732800000.jpg',
                   model: '2021',
                   daily_price: 150,
-                  description: 'The BMW 3 Series is a compact executive car manufactured by the German automaker BMW since May 1975. It is the successor to the 02 Series and has been produced in six different generations.')
+                  description: 'The BMW 3 Series is a compact executive car.')
     @booking = Booking.new(pickup_date: '2023-01-05', return_date: '2023-01-08')
     @booking.car = car
     @booking.user = user
