af12d6c2f1a4143b3fe250f19483055bdb82ce92
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-06 12:17:45 +0300

    Fix linter errors 🩹

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Booking, type: :model do
   before :each do
     user = User.new(name: 'Abel.G', email: 'abcd@gmail.com', password: '123456')
     car = Car.new(name: 'BMW 3 Series',
-      image: 'https://www.bmw.com/content/dam/bmw/common/all-models/3-series/sedan/2021/navigation/BMW-3-Series-Sedan-2021-Exterior-01.jpg/_jcr_content/renditions/cq5dam.resized.img.585.low.time1594732800000.jpg',
-      model: '2021',
-      daily_price: 150,
-      description: 'The BMW 3 Series is a compact executive car manufactured by the German automaker BMW since May 1975. It is the successor to the 02 Series and has been produced in six different generations.')
+                  image: 'https://www.bmw.com/content/dam/bmw/common/all-models/3-series/sedan/2021/navigation/BMW-3-Series-Sedan-2021-Exterior-01.jpg/_jcr_content/renditions/cq5dam.resized.img.585.low.time1594732800000.jpg',
+                  model: '2021',
+                  daily_price: 150,
+                  description: 'The BMW 3 Series is a compact executive car manufactured by the German automaker BMW since May 1975. It is the successor to the 02 Series and has been produced in six different generations.')
     @booking = Booking.new(pickup_date: '2023-01-05', return_date: '2023-01-08')
     @booking.car = car
     @booking.user = user
