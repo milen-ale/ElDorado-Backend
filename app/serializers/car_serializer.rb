98c9eb720101ccb6d89c6e3ad9bdcfdc2465cbbd
milen-ale <milenalebel2@gmail.com>
2023-01-13 21:45:33 +0300

    modify serilizer

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,3 +1,3 @@
 class CarSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :model, :daily_price, :description
+  attributes :id, :name, :image, :model, :daily_price, :description, :available
 end
