0462369ccad2a322e9a7156b5e64867a934c2887
milen-ale <milenalebel2@gmail.com>
2023-01-13 21:32:10 +0300

    add owner_cars to the car  controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,18 +1,43 @@
 class Api::V1::CarsController < ApplicationController
-  before_action :set_car, only: [:show]
+  before_action :set_car, only: %i[show update availability]
+  before_action :authenticate_user!, only: %i[create update availability owner_cars]
 
   def index
-    @cars = Car.all
+    @cars = Car.where(available: true)
     render json: @cars, status: :ok
+  end
+
+  def owner_cars
+    @owner_cars = Car.where(user: current_user)
+    render json: @owner_cars, status: :ok
   end
 
   def show
     render json: @car, status: :ok
   end
 
-  private
+  def create
+    car = Car.new(car_params)
+    if car.save!
+      render json: {
+        status: 201,
+        message: 'Car has been successfully created',
+        data: CarSerializer.new(car)
+      }, status: :created
+    else
+      render json: { error: 'ERROR: Unable to create the car' }, status: :unprocessable_entity
+    end
+  end
+ private
 
   def set_car
     @car = Car.find(params[:id])
   end
+
+  def car_params
+    params.require(:car)
+      .permit(:name, :model, :image, :daily_price, :description, :available)
+      .with_defaults(user_id: current_user.id)
+  end
+
 end
