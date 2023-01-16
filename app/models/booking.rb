70bf00ff031f8a6095eed8a4bacdd855ce722fd3
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-16 21:12:46 +0500

    Fix validation for booking model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,15 +2,15 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  validates :car_id, presence: true, uniqueness: { scope: :user_id, message: 'You have already booked this car' }
+  validates :car_id, presence: true, uniqueness: { scope: :user_id, message: 'has already been booked' }
   validates :user_id, presence: true
   validates :return_date, comparison: { greater_than: :pickup_date }
   validates :pickup_date, presence: true,
                           comparison: { greater_than_or_equal_to: Date.today,
-                                        message: 'Pickup date must be today or later' }
+                                        message: 'must be today or later' }
   validates :return_date, presence: true,
-                          comparison: { greater_than: Date.today + 1.day,
-                                        message: 'Return date must be at least 1 day after pickup date' }
+                          comparison: { greater_than: Date.today,
+                                        message: 'must be at least 1 day after pickup date' }
 
   validate :car_not_owned_by_user
   validate :car_not_found
@@ -18,10 +18,10 @@ class Booking < ApplicationRecord
   private
 
   def car_not_owned_by_user
-    errors.add(:car_id, 'You cannot book your own car') if car.present? && car.user == user
+    errors.add(:car_id, 'cannot be booked by the same owner') if car.present? && car.user == user
   end
 
   def car_not_found
-    errors.add(:car_id, 'Car not found') if car.nil?
+    errors.add(:car_id, 'not found') if car.nil?
   end
 end
