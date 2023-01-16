dc544f75d2eef09e1679bfd0bb3bc620d7a49517
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-16 20:54:55 +0500

    Fix car name validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings, dependent: :destroy
 
-  validates :name, presence: true, length: { in: 4..250 }, uniqueness: true
+  validates :name, presence: true, length: { in: 4..250 }
   validates :model, presence: true, length: { in: 4..250 }
   validates :image, presence: true, length: { in: 4..250 }, uniqueness: true
   validates :daily_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
